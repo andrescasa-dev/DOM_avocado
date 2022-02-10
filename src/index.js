12bc76b2886dac67d46e8fb9207a6fabe3d318ee
andrescasa-dev <andrescasa.dev@gmail.com>
2022-02-10 10:25:44 -0500

    fetching data using promises

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const API_URL = "https://platzi-avo.vercel.app/api/avo";
 
-console.log('Happy hacking :)')
+fetch(API_URL).then(response => response.json().then(responseJson => {
+  const nodes = [];
+  responseJson.data.forEach( avocado => {
+    const img = document.createElement('img');
+    
+    const title = document.createElement('h2');
+    
+    const price = document.createElement('div');
+
+    const container = document.createElement('div');
+    container.append(img, title, price);
+    nodes.push(container);
+  });
+  document.body.append(...nodes);
+}));
