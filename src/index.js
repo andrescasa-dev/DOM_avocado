b99381b94b6a883f425d4b2308c3adae9c81635f
andrescasa-dev <andrescasa.dev@gmail.com>
2022-02-22 08:01:36 -0500

    the font family was changed a

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const baseUrl = "https://platzi-avo.vercel.app";
 const nodeApp = document.getElementById('app');
 
+/**
+ * 
+ * @param {number_price} price 
+ * @returns String which has the US currency format.
+ */
 const formatPrice = price =>{
   return new window.Intl.NumberFormat('en-US',{
     currency: "USD",
@@ -22,14 +27,14 @@ async function displayData(){
   responseJson.data.forEach(avocado => {
     const img = document.createElement('img');
     img.src = `${baseUrl}${avocado.image}`;
-    img.className = "rounded-full";
 
     const title = document.createElement('h2');
     title.className = "txt-md";
     title.textContent = avocado.name;
 
-    const price = document.createElement('div');
+    const price = document.createElement('p');
     price.textContent = formatPrice(avocado.price);
+    price.style.marginBlockStart = "10px"
     price.className = "txt-md";
 
     const priceAndTile = document.createElement('div');
@@ -40,12 +45,16 @@ async function displayData(){
     card.append(img, priceAndTile);
     card.className = "card";
 
+    //I use fragment for avoid calling so many times to append function.
     fragment.appendChild(card);
   });
+
   //Since there are few avocados, I copy the group of avocados in order to show the responsive design.
   const copyFragment = fragment.cloneNode(true);
-  nodeApp.append(fragment);
   nodeApp.append(copyFragment);
+
+  //I don't append each avocado, instead, I append a group of avocados.
+  nodeApp.append(fragment);
 }
 
 displayData();
