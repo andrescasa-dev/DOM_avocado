f0e7df70ebabf145d5cd31842d1a16ad62894f98
andrescasa-dev <andrescasa.dev@gmail.com>
2022-03-01 08:03:58 -0500

    attempt two deployment

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,6 +5,6 @@ module.exports = {
     src: '/_dist_',
   },
   buildOptions: {
-    baseUrl: 'https://andrescasa-dev.github.io/DOM_avocado/',
+    baseUrl: 'https://andrescasa-dev.github.io/DOM_avocado',
   },
 }
